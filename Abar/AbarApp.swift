ada148f5353c67a467a13c782071520ece414265
Haifa <haifabnf@gmail.com>
2024-01-17 15:18:28 +0300

    Initial Commit

---
+++ Abar/AbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbarApp.swift
+//  Abar
+//
+//  Created by haifa alfoiz on 17/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct AbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
