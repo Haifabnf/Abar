c2e4312395436ce61cb6a661dbe77b841b9487df
Haifa <haifabnf@gmail.com>
2024-01-24 02:19:50 +0300

    hh

--- Abar/ContentView.swift
+++ Abar/ContentView.swift
@@ -6,9 +6,12 @@
 //
 
 import SwiftUI
+import EventKit
+import EventKitUI
 
 struct ContentView: View {
-    
+        
+    @State private var wakeUp = Date.now
 
     var body: some View {
         NavigationView{
@@ -18,7 +21,18 @@ struct ContentView: View {
                             Text("Next Page")
                     
                 }
-                
+                Form{
+                    Text("gg")
+                    DatePicker("Select Date", selection: $wakeUp, in: Date()...)
+                        .background(.white)
+                        .cornerRadius(10)
+                        .accentColor(.purple)
+                        //.datePickerStyle(.graphical)
+                                       
+                        //.datePickerStyle(.compact)
+                        //.datePickerStyle(.wheel)
+                        //.datePickerStyle(.automatic)
+                }
                 
             }
         }
--- Abar/Details.swift
+++ Abar/Details.swift
@@ -12,52 +12,96 @@ struct Details: View {
     @State private var recieverName: String = ""
     @State private var recieverNumber: String = ""
     
+    @State private var wakeUp = Date.now
+
     
     var body: some View {
         
-        NavigationStack{
+        NavigationView{
             
-            ScrollView{
-                
+            ZStack {
+                Color(.f3)
+                    .ignoresSafeArea()
                 VStack{
-                    
+
                     //Card Preview
-                    Rectangle()
-                    .frame(width: 304, height: 357)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-                    .shadow(color: Color(red: 0.5, green: 0.5, blue: 0.5).opacity(0.25), radius: 20, x: 10, y: 10)
-                    .padding()
-                    
-                    HStack{
-                        Text("Reciever Details")
-                            //.font(Font.custom("SF Pro", size: 18))
-                            .font(.custom("SF Pro", size: 22))
-                            .fontWeight(.bold)
-                            .foregroundColor(.black)
+                        Rectangle()
+                            .frame(width: 300, height: 360)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .shadow(color: Color(red: 0.5, green: 0.5, blue: 0.5).opacity(0.25), radius: 10, x: 10, y: 10)
+                            .padding()
                         
-                        Spacer()
-                    }
-                    .padding()
-                    
-                    List {
-                        Section {
+                            
+                    List{
+                        Section(header: CustomText("Reciever Details")){
                             TextField("Reciever Name", text: $recieverName)
-                            TextField("Reciever Number", text: $recieverNumber)                        }
+                                .background(.white)
+                                .cornerRadius(10)
+                                .foregroundColor(.black)
+                                .padding(.horizontal)
+                            
+                            TextField("Reciever Number", text: $recieverNumber)
+                                .background(.white)
+                                .cornerRadius(10)
+                                .foregroundColor(.black)
+                                .padding(.horizontal)
+                        }
+                    
+            
+                    
+                        Section(header: CustomText("Schedule Time")){
+                            DatePicker("Select Date", selection: $wakeUp, in: Date()...)
+                                .background(.white)
+                                .cornerRadius(10)
+                                .accentColor(.purple)
+                                //.datePickerStyle(.graphical)
+                                //.datePickerStyle(.compact)
+                                //.datePickerStyle(.wheel)
+                                .datePickerStyle(.automatic)
+                            
+                        }
+
                     }
+                    .listStyle(PlainListStyle())
+                    .background(Color.f3)
+                    
+                    NavigationLink(destination: ContentView()) {
+                        
+                        Button("Done"){
+                            
+                        }
+                        .padding()
+                        .frame(width: 339 , height: 60)
+                        .background(Color.purplee)
+                        .cornerRadius(20)
+                        .foregroundColor(.white)
+                        .font(.custom("SF Pro", size: 20))
+                    }
+                }
+            }
+
                 
 
-                    
-                    Spacer()
-                }
-                .padding()
-                .textFieldStyle(.roundedBorder)
-
-
-            }
         }
         .navigationTitle("Send Details")
         
+    }
+}
+
+struct CustomText: View {
+    var text: String
+    
+    init(_ text: String) {
+        self.text = text
+    }
+    
+    var body: some View {
+        Text(text)
+            .font(.custom("SF Pro", size: 22))
+            .foregroundColor(.black)
+            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            .textCase(.none)
     }
 }
 
