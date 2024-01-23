b3405385ff2080dd5076734b90c21573acf08a08
Haifa <haifabnf@gmail.com>
2024-01-23 18:48:11 +0300

    ..

--- Abar/ContentView.swift
+++ Abar/ContentView.swift
@@ -8,14 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            VStack {
+                
+                NavigationLink(destination: Details()) {
+                            Text("Next Page")
+                    
+                }
+                
+                
+            }
         }
-        .padding()
     }
 }
 
--- Abar/Details.swift
+++ Abar/Details.swift
@@ -8,8 +8,56 @@
 import SwiftUI
 
 struct Details: View {
+    
+    @State private var recieverName: String = ""
+    @State private var recieverNumber: String = ""
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationStack{
+            
+            ScrollView{
+                
+                VStack{
+                    
+                    //Card Preview
+                    Rectangle()
+                    .frame(width: 304, height: 357)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    .shadow(color: Color(red: 0.5, green: 0.5, blue: 0.5).opacity(0.25), radius: 20, x: 10, y: 10)
+                    .padding()
+                    
+                    HStack{
+                        Text("Reciever Details")
+                            //.font(Font.custom("SF Pro", size: 18))
+                            .font(.custom("SF Pro", size: 22))
+                            .fontWeight(.bold)
+                            .foregroundColor(.black)
+                        
+                        Spacer()
+                    }
+                    .padding()
+                    
+                    List {
+                        Section {
+                            TextField("Reciever Name", text: $recieverName)
+                            TextField("Reciever Number", text: $recieverNumber)                        }
+                    }
+                
+
+                    
+                    Spacer()
+                }
+                .padding()
+                .textFieldStyle(.roundedBorder)
+
+
+            }
+        }
+        .navigationTitle("Send Details")
+        
     }
 }
 
