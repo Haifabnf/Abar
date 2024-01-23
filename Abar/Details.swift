9e804b79b0545be4c9668c73acad412e83867d51
Haifa <haifabnf@gmail.com>
2024-01-23 18:47:24 +0300

    c

---
+++ Abar/Details.swift
@@ -0,0 +1,18 @@
+//
+//  Details.swift
+//  Abar
+//
+//  Created by haifa alfoiz on 23/01/2024.
+//
+
+import SwiftUI
+
+struct Details: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Details()
+}
