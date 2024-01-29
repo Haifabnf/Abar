303bd083d3721a9beac53b4de1bb2353a006b546
reemaalsmari <alsmarireema@gmail.com>
2024-01-29 14:52:18 +0300

    main page ( done )

--- Abar/ContentView.swift
+++ Abar/ContentView.swift
@@ -16,19 +16,28 @@ struct ContentView: View {
     
     var body: some View {
         
-        //        var filteredIndices: [Int] {
-        //            if searchTerm.isEmpty {
-        //                return Array(0..<textArray.count)
-        //            } else {
-        //                return textArray.indices.filter {
-        //                    let localizedString = textArray[$0]
-        //                    let regularString = String(describing: localizedString)
-        //                    return regularString.lowercased().contains(searchTerm.lowercased())
-        //                }
-        //            }
-        //        }
+        let customGradient = LinearGradient(
+            gradient: Gradient(colors: [Color("StartColor"), Color("white")]),
+            startPoint: .leading,
+            endPoint: .trailing )
         
+        let textArray = ["Happy Birthday", "Graduation", "New Job", "Memes", "Artist", "Gratitude"]
         
+        let imageNameArray1 = ["Balloon", "Graduation", "Clapping", "Sticker", "Mic", "choking", "Flower"]
+        
+        let imageNameArray = ["balloon23", "grad23", "clap23", "sticker23", "mic23", "flower23"]
+        
+        var filteredIndices: [Int] {
+            if searchTerm.isEmpty {
+                return Array(0..<textArray.count)
+            } else {
+                return textArray.indices.filter {
+                    let localizedString = textArray[$0]
+                    let regularString = String(describing: localizedString)
+                    return regularString.lowercased().contains(searchTerm.lowercased())
+                }
+            }
+        }
         
         NavigationView {
             ScrollView {
@@ -63,154 +72,114 @@ struct ContentView: View {
                     Text("Special Ocassions")
                         .font(.title2)
                         .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding()
+                        .padding(.horizontal)
+                    
                     
                     
                     ScrollView(.horizontal, showsIndicators: false) {
-                                  HStack(spacing: 20) {
-                                      RectangleView(imageName: "image1", title: "Rectangle 1", destination: Text("Page 1"))
-                                      RectangleView(imageName: "image2", title: "Rectangle 2", destination: Text("Page 2"))
-                                      RectangleView(imageName: "image3", title: "Rectangle 3", destination: Text("Page 3"))
-                                  }
-                                  .padding()
-                              }
-                    
+                        HStack(spacing: 20) {
+                            RectangleView(imageName: "foundationDay",  destination: Text("Page 1"))
+                            RectangleView(imageName: "Yarb",
+                                          destination: Text("Page 2"))
+                            
+                        }
+                        .padding(.horizontal)
+                    }
                     
                     
                     Text("Occasions")
                         .font(.title2)
                         .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding()
+                        .padding(.horizontal)
                     
-                    LazyVGrid(columns: [GridItem(.flexible(), spacing: 0), GridItem(.flexible(), spacing: 0)]) {
-                        ForEach(1..<7) { index in
-                            createNavigationLink(index: index)
+                    
+                    LazyVGrid(columns: [GridItem(.flexible(), spacing: -30), GridItem(.flexible(), spacing: -30)]) {
+                        
+                        ForEach(filteredIndices, id: \.self)  { index in
+                            // Using NavigationLink to create a navigation link for each rectangle
+                            NavigationLink(destination: DetailView(index: index)) {
+                                
+                                HStack {
+                                    
+                                    Rectangle()
+                                        .fill(customGradient)
+                                        .frame(width: 171, height: 117)
+                                        .padding(1)
+                                        .cornerRadius(10)
+                                        .overlay (
+                                            
+                                            VStack {
+                                                
+                                                Text(textArray[index])
+                                                    .foregroundColor(.black)
+                                                    .fontWeight(.regular)
+                                                    .font(.subheadline)
+                                                    .padding()
+                                                
+                                                Spacer()
+                                                
+                                                
+                                                Image("Balloon")
+                                                    .resizable()
+                                                    .frame(width: 42, height: 46)
+                                                    .frame(alignment: .trailing)
+                                                    .offset(x: 70, y: -15)
+                                            }
+                                            
+                                            , alignment: .topLeading)
+                                }
+                            }
                         }
                     }
-                    
                     
                 }
                 .navigationTitle("Explore")
                 
                 
-              
             } .background(
                 LinearGradient(
-                stops: [
-                Gradient.Stop(color: Color(red: 0.5, green: 0.36, blue: 0.64), location: 0.07),
-                Gradient.Stop(color: Color(red: 0.71, green: 0.84, blue: 0.9), location: 0.40),
-                Gradient.Stop(color: Color(red: 0.95, green: 0.95, blue: 0.95), location: 0.89),
-                ],
-                startPoint: UnitPoint(x: 0.5, y: -0.26),
-                endPoint: UnitPoint(x: 0.5, y: 0.46)
+                    stops: [
+                        Gradient.Stop(color: Color(red: 0.5, green: 0.36, blue: 0.64), location: 0.07),
+                        Gradient.Stop(color: Color(red: 0.71, green: 0.84, blue: 0.9), location: 0.40),
+                        Gradient.Stop(color: Color(red: 0.95, green: 0.95, blue: 0.95), location: 0.89),
+                    ],
+                    startPoint: UnitPoint(x: 0.5, y: -0.26),
+                    endPoint: UnitPoint(x: 0.5, y: 0.46)
                 )
-                )
+            )
         }
         
     }
 }
 
 
+struct DetailView: View {
+    // Parameter to receive the index from the ContentView
+    let index: Int
+    
+    var body: some View {
+        // Using the index to display specific data for each rectangle
+        Text("Detail for Rectangle \(index)")
+            .navigationTitle("Rectangle \(index)")
+    }
+}
+
 struct RectangleView: View {
     var imageName: String
-    var title: String
     var destination: Text
     
     var body: some View {
         NavigationLink(destination: destination) {
+            
             Rectangle()
                 .foregroundColor(.clear)
-                .background(Color(.systemGray5))
+                .background( Image(imageName) )
                 .frame(width: 214, height: 117)
                 .cornerRadius(10)
-                .overlay(
-                    Text(title)
-                        .foregroundColor(.black)
-                )
+            
         }
     }
 }
-
-func createNavigationLink(index: Int) -> some View {
-    let content: String
-    
-    switch index {
-    case 1:
-        content = "rec 1"
-    case 2:
-        content = "rec 2"
-    case 3:
-        content = "rec 3"
-    case 4:
-        content = "rec 4"
-    case 5:
-        content = "rec 5"
-    case 6:
-        content = "rec 6"
-    default:
-        content = "Unknown Page Content"
-    }
-
-    return NavigationLink(destination: DetailView(content: content)) {
-        createRectangle(index: index, content: content)
-    }
-    .buttonStyle(PlainButtonStyle())
-}
-
-func createRectangle(index: Int, content: String) -> some View {
-    Rectangle()
-        .foregroundColor(.clear)
-        .frame(width: 171, height: 117)
-        .background(Color(.systemGray5))
-        .cornerRadius(10)
-        .overlay(Text(content).foregroundColor(.black))
-        .padding()
-}
-
-
-struct DetailView: View {
-let content: String
-
-var body: some View {
-    VStack {
-        Text(content)
-            .padding()
-
-        // Customize additional content for each page
-        switch content {
-        case "rec 1":
-            Text("yeees this is page 1")
-                .padding()
-            
-        case "rec 2":
-            Text("page 2 !!!")
-                .padding()
-            
-        case "rec 3":
-            Text("nononono this page 3 ")
-                .padding()
-            
-        case "rec 4":
-            Text("Page 4 wohoo")
-                .padding()
-            
-        case "rec 5":
-            Text("yep yep page 5")
-                .padding()
-            
-        case "rec 6":
-            Text("page 6 laast")
-                .padding()
-            
-        default:
-            Text("Unknown Page Content").padding()
-        }
-    }
-    .navigationBarTitle("Detail Page")
-}
-}
-
-
 
 #Preview {
     ContentView()
