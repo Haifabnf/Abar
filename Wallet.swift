216ffc5c45fa992eaf8a7ac45ca6839f52573c1c
Haifa <haifabnf@gmail.com>
2024-01-31 15:13:05 +0300

    edits

---
+++ Wallet.swift
@@ -0,0 +1,18 @@
+//
+//  Wallet.swift
+//  Abar
+//
+//  Created by haifa alfoiz on 28/01/2024.
+//
+
+import SwiftUI
+
+struct Wallet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Wallet()
+}
