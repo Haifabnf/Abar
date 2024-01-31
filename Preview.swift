a84a288c544ba4e91206d6c086d5182762817df2
Haifa <haifabnf@gmail.com>
2024-01-31 16:53:52 +0300

    prev

---
+++ Preview.swift
@@ -0,0 +1,18 @@
+//
+//  Preview.swift
+//  Abar
+//
+//  Created by haifa alfoiz on 31/01/2024.
+//
+
+import SwiftUI
+
+struct Preview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Preview()
+}
