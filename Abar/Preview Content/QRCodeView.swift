511eeb20f09dd2fb97b383a79731e950cd5d94cf
Haifa <haifabnf@gmail.com>
2024-02-08 16:10:19 +0300

    hh

---
+++ Abar/Preview Content/QRCodeView.swift
@@ -0,0 +1,18 @@
+//
+//  QRCodeView.swift
+//  Abar
+//
+//  Created by haifa alfoiz on 08/02/2024.
+//
+
+import SwiftUI
+
+struct QRCodeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QRCodeView()
+}
---
+++ Abar/PreviewView.swift
@@ -0,0 +1,18 @@
+//
+//  PreviewView.swift
+//  Abar
+//
+//  Created by haifa alfoiz on 08/02/2024.
+//
+
+import SwiftUI
+
+struct PreviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PreviewView()
+}
